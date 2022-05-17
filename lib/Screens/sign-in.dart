b183c70dafaaa08fd4ee41635352bb1e598b9203
harikamaljana <janaharikamal@gmail.com>
2022-05-17 12:53:20 -0500

    Initial Commit

---
+++ lib/Screens/sign-in.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SignInScreen extends StatefulWidget {
+  const SignInScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SignInScreen> createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+              Color.fromARGB(255, 167, 206, 239),
+              Color.fromARGB(255, 44, 90, 130)
+            ])),
+      ),
+    );
+  }
+}
